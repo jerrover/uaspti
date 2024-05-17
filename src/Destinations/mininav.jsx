ffd3cecb236a72fcd851ceae563e5eb65d08c8f4
jerrover <jerrover@gmail.com>
2024-05-17 12:00:28 +0700

    jupd

--- src/Destinations/mininav.jsx
+++ src/Destinations/mininav.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-import WisataBersejarah from "./sejarah";
-import WisataGunungList from "./gunung";
-import WisataAirTerjun from "./airterjun";
-import WisataPantaiList from "./pantai";
 import "./mininav.css";
 
 function Mininav() {
@@ -15,24 +10,12 @@ function Mininav() {
           <li className="mn-li">
             <Link to="/destination/terjun" className="mn-a">Air Terjun</Link>
           </li>
-        </ul>
-      </nav>
-      <nav className="mn-nav">
-        <ul className="mn-ul">
           <li className="mn-li">
             <Link to="/destination/gunung" className="mn-a">Gunung</Link>
           </li>
-        </ul>
-      </nav>
-      <nav className="mn-nav">
-        <ul className="mn-ul">
           <li className="mn-li">
             <Link to="/destination/situs" className="mn-a">Situs Bersejarah</Link>
           </li>
-        </ul>
-      </nav>
-      <nav className="mn-nav">
-        <ul className="mn-ul">
           <li className="mn-li">
             <Link to="/destination/pantai" className="mn-a">Pantai</Link>
           </li>
