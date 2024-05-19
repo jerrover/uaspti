694a0d0893565e7bd877cd954ec8796e33c3a725
jerrover <jerrover@gmail.com>
2024-05-19 21:20:23 +0700

    Update Extras.jsx

--- src/Pages/Extras.jsx
+++ src/Pages/Extras.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Extras.css";
 
@@ -38,6 +38,10 @@ const Extras = () => {
   const [extraLoading, setExtraLoading] = useState(false);
   const [sortBy, setSortBy] = useState("");
   const [cityImage, setCityImage] = useState(null);
+  
+  useEffect(() => {
+    handleExtraImageSearch();
+  }, [sortBy]);
 
   const handleExtraImageSearch = async () => {
     try {
